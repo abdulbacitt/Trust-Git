92564e1f5179cdf0337ed0e05abe34bfeb35530d
HP1717\91700 <raja_2020bece042@nitsri.net>
2022-08-15 16:25:40 +0530

    this a new second local commit

--- potential ist file.cpp
+++ potential ist file.cpp
@@ -5,5 +5,6 @@ int main()
     cout<<" Hello world";
     cout<< " Is this a seconde change";
     cout << " I'm making a local change from VS ";
+    cout << " This is a second change locally and I'm doing a favor on myself";
     return 0;
 }
